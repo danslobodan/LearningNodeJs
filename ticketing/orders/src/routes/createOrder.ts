58b4532322a32e955e7c79e791f74f0153107b4b
Slobodan Dan <danastroman@gmail.com>
2021-04-28 21:48:52 +0200

    Added validation to create order route.

--- ticketing/orders/src/routes/createOrder.ts
+++ ticketing/orders/src/routes/createOrder.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import { requireAuth, validateRequest } from '@sdtickets/common';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.post(
+    '/api/orders',
+    requireAuth,
+    [body('ticketId').not().isEmpty().withMessage('TicketId must be provided')],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const {} = req.body;
+
+        res.send({});
+    }
+);
 
 export { router as createOrderRouter };
