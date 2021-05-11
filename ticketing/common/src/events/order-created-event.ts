515de31caae11bf2d81a020e8243128921c9d532
Slobodan Dan <danastroman@gmail.com>
2021-05-11 21:11:20 +0200

    Fixed typo in order created event.

--- ticketing/common/src/events/order-created-event.ts
+++ ticketing/common/src/events/order-created-event.ts
@@ -3,7 +3,7 @@ import { OrderStatus } from './types/order-status';
 
 export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated;
-    date: {
+    data: {
         id: string;
         status: OrderStatus;
         userId: string;
