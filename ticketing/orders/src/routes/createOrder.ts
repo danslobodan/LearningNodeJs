9b998ee0ec01d5a686ae0ee4a25ee08d137b0b19
Slobodan Dan <danastroman@gmail.com>
2021-05-12 21:57:13 +0200

    Added order created event.

--- ticketing/orders/src/routes/createOrder.ts
+++ ticketing/orders/src/routes/createOrder.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 import {
     BadRequestError,
@@ -44,6 +46,17 @@ router.post(
             ticket,
         });
         await order.save();
+
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            status: order.status,
+            userId: order.userId,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price,
+            },
+        });
 
         res.status(201).send(order);
     }
